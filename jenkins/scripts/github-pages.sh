6a3394490d95c58f8b57106a0da29e7ff0ac3a3a
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2023-08-06 16:50:59 +0700

    use apt-get

--- jenkins/scripts/github-pages.sh
+++ jenkins/scripts/github-pages.sh
@@ -3,7 +3,7 @@ set -x
 # just to ensure the build
 ls build
 # install git (optional if you already have it)
-sudo apt install git-all
+apt-get update && apt-get install -y git-all
 # set the target repo
 git remote set-url origin https://git:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
 ## run gh-pages, please ensure the jenkins have the permission
