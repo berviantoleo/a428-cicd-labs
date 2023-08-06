9d2f9a9c8a7a045ab33417527c788fe19ceba46c
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2023-08-06 16:20:30 +0700

    add gh-pages

--- jenkins/scripts/github-pages.sh
+++ jenkins/scripts/github-pages.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env sh
-ls
+# just to ensure the build
+ls build
+# use gh-pages npm
+npm install -g --silent gh-pages@5.0.0
+## run gh-pages, please ensure the jenkins have the permission
+gh-pages --message '[skip ci] Updates' --dist build
